479193bd65716a6ccbcd3554300684f49540bf89
Prasanth <prasanthemmani@gmail.com>
2025-08-15 19:12:11 +0530

    URL correction

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,33 @@ import Player from "./pages/Player";
 import Comparison from "./pages/Comparison";
 import NotFound from "./NotFound";
 
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: <Navigate to="/home" replace /> },
+  const router = createBrowserRouter(
+    [
+      { path: "/", element: <Navigate to="/home" replace /> },
+      {
+        path: "/",
+        element: <DashboardLayout />,
+        children: [
+          { path: "home", element: <Home /> },
+          { path: "player-stats", element: <Player /> },
+          { path: "players-comparison", element: <Comparison /> },
+          { path: "*", element: <NotFound /> },
+        ],
+      },
+    ],
     {
-      path: "/",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/home",
-          element: <Home />,
-        },
-        {
-          path: "/player-stats",
-          element: <Player />,
-        },
-        {
-          path: "/players-comparison",
-          element: <Comparison />,
-        },
-        { path: "*", element: <NotFound /> },
-      ],
-    },
-  ]);
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
+      basename: "/cricket-insights",
+    }
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
